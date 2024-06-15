6dd65a8c8c19e8019afd5875066fa2068ad7035e
Adanan Chouhan <chouhanarbaaz4@gmail.com>
2024-06-15 10:24:06 +0530

    JavaScript-1-Tutorials-Playlist

--- js/13-Assignment-operators.js
+++ js/13-Assignment-operators.js
@@ -1,36 +1,36 @@
-//Assignment operators
+// //Assignment operators
 
-var a = 24;
-a += 36;
-document.write(a);
-document.write("<br>");
+// var a = 24;
+// a += 36;
+// document.write(a);
+// document.write("<br>");
 
-var b = 134;
-b -= 36;
-document.write(b);
-document.write("<br>");
+// var b = 134;
+// b -= 36;
+// document.write(b);
+// document.write("<br>");
 
-var c = 24;
-c *= 10;
-document.write(c);
-document.write("<br>");
+// var c = 24;
+// c *= 10;
+// document.write(c);
+// document.write("<br>");
 
-var d = 24; 
-d /= 4;
-document.write(d);
-document.write("<br>");
+// var d = 24; 
+// d /= 4;
+// document.write(d);
+// document.write("<br>");
 
-var e = 100; 
-e %= 40;
-document.write(e);
-document.write("<br>");
+// var e = 100; 
+// e %= 40;
+// document.write(e);
+// document.write("<br>");
 
-var bucky = 30;
-bucky += 39;
-bucky -= 94;
-bucky *= 10;
-bucky /= 3;
+// var bucky = 30;
+// bucky += 39;
+// bucky -= 94;
+// bucky *= 10;
+// bucky /= 3;
 
-document.write(bucky);
+// document.write(bucky);
 
 
